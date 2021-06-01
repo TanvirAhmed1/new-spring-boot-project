9fcf0874874ca9f1ea64aa53c7b12f60333b1aee
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-06-02 00:56:38 +0600

    Initial Commit

---
+++ src/test/java/com/example/new_spring_boot_project/NewSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
