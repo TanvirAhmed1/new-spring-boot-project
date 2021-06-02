9d6b139878b6cfb255414d5ef521f6eff76e4cde
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-06-03 01:43:16 +0600

    Simple API Created

--- src/main/java/com/example/new_spring_boot_project/NewSpringBootProjectApplication.java
+++ src/main/java/com/example/new_spring_boot_project/NewSpringBootProjectApplication.java
@@ -1,13 +1,23 @@
 package com.example.new_spring_boot_project;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewSpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewSpringBootProjectApplication.class, args);
+	}
+	
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
